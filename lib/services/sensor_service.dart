1c4ef63f1cdd9bce06fd519dd1f14e3184ab608d
Eduardo Vega <eduar05066@gmail.com>
2025-11-21 21:03:51 -0500

    Los sensores estan registrando

--- lib/services/sensor_service.dart
+++ lib/services/sensor_service.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:sensors_plus/sensors_plus.dart';
+import 'dart:math' as math;
 // import 'package:flutter_barometer/flutter_barometer.dart'; // Paquete incompatible con Android Gradle moderno
 
 class SensorService {
@@ -15,10 +16,22 @@ class SensorService {
   List<double> magnetometer = [0, 0, 0];
   double barometer = 0;
 
+  double posX = 0;
+  double posY = 0;
+  double heading = 0;
+
+  double stepLength = 0.6; // Longitud promedio de paso en metros
+
   Function()? onDataChanged;
   
   // Timer para imprimir valores periódicamente
   // Timer? _printTimer;
+  void _onStepDetected(){
+    posX += stepLength * math.cos(heading);
+    posY += stepLength * math.sin(heading);
+
+    print("Paso detectado -> x: $posX , y: $posY , heading: $heading");
+  }
 
   void start() {
     // Iniciar timer para imprimir valores cada segundo
@@ -29,12 +42,20 @@ class SensorService {
     //Acelerómetro
     _accelSub = accelerometerEventStream().listen((event) {
       accelerometer = [event.x, event.y, event.z];
+
+      //Detectar paso con un pico de aceleracion
+      if(event.z > 12 ){
+        _onStepDetected();
+      }
       _notify();
     });
 
     //Giroscopio
     _gyroSub = gyroscopeEventStream().listen((event) {
       gyroscope = [event.x, event.y, event.z];
+
+      //Sumar la rotacion al heading
+      heading += event.z * 0.02; // factor de ajuste
       _notify();
     });
 
